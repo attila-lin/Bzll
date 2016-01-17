b8f227678dc8e889fb5c8908c178ef99ef36385c
Yuyi Lin <linyiyu1992@gmail.com>
2016-01-17 10:10:44 +0800

    fps

--- XcodeGlutDemo/XcodeGlutDemo/main.cpp
+++ XcodeGlutDemo/XcodeGlutDemo/main.cpp
@@ -21,13 +21,54 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
+typedef struct
+{
+    // Handle to a program object
+    GLuint programObject;
+} UserData;
+
+GLuint LoadShader ( GLenum type, const char *shaderSrc )
+{
+    GLuint shader;
+    GLint compiled;
+    
+    // Create the shader object
+    shader = glCreateShader ( type );
+    if ( shader == 0 )
+        return 0;
+    // Load the shader source
+    glShaderSource ( shader, 1, &shaderSrc, NULL );
+    // Compile the shader
+    // TODO 预编译
+    glCompileShader ( shader );
+    // Check the compile status
+    glGetShaderiv ( shader, GL_COMPILE_STATUS, &compiled );
+    if ( !compiled )
+    {
+        GLint infoLen = 0;
+        glGetShaderiv ( shader, GL_INFO_LOG_LENGTH, &infoLen );
+        if ( infoLen > 1 )
+        {
+            char* infoLog = (char *)malloc (sizeof(char) * infoLen );
+            glGetShaderInfoLog( shader, infoLen, NULL, infoLog );
+            printf ( "Error compiling shader:\n%s\n", infoLog );
+            free ( infoLog );
+        }
+        glDeleteShader ( shader );
+        return 0;
+    }
+    return shader;
+}
+
 int main(void)
 {
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    
+    window = glfwCreateWindow(1280, 720, "Simple example", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -36,8 +77,27 @@ int main(void)
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
     glfwSetKeyCallback(window, key_callback);
+    
+    // 添加鼠标
+    // GLFWcursor* cursor = glfwCreateCursor(")
+    std::cout << glfwGetVersionString() << std::endl;
+    
+    // FPS
+    // http://www.opengl-tutorial.org/miscellaneous/an-fps-counter/
+    double lastTime = glfwGetTime();
+    int nbFrames = 0;
+    
     while (!glfwWindowShouldClose(window))
     {
+        
+        double currentTime = glfwGetTime();
+        nbFrames++;
+        if ( currentTime - lastTime >= 1.0 ){
+            printf("%f ms/frame\n", 1000.0/double(nbFrames));
+            nbFrames = 0;
+            lastTime += 1.0;
+        }
+        
         /* Render here */
         float ratio;
         int width, height;
