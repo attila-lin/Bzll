151ea6632b6d450f00928a29528be1dabbf38001
Yiyu Lin <linyiyu1992@gmail.com>
2017-01-17 21:43:00 +0800

    add filesystem

---
+++ src/filesystem.rs
@@ -0,0 +1,49 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
+
+// enum FileStatus
+// {
+// 	Ok,           			
+// 	IOError,          		
+// 	EOS,              		
+// 	IllegalCall,      		
+// 	Closed,           		
+// 	UnknownError
+// }
+
+pub fn open(p: &str) -> ::std::string::String {
+	let path = Path::new(p);
+	let display = path.display();
+
+	// Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", display,
+                                                   why.description()),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display,
+                                                   why.description()),
+        Ok(_) => s,
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+	use filesystem;
+	#[test]
+	#[should_panic]
+    fn valid_gles_versions() {
+        // only irregular version
+        assert_eq!(filesystem::open("heheh"),"hh");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
+#![warn(non_snake_case)]
+
+
 #[macro_use]
 extern crate glium;
 extern crate image;
+
+mod filesystem;
 
 use std::io::Cursor;
 
@@ -41,63 +48,12 @@ fn main() {
     let normal_map = glium::texture::Texture2d::new(&display, image).unwrap();
 
 
-    let vertex_shader_src = r#"
-        #version 150
-        in vec3 position;
-        in vec3 normal;
-        in vec2 tex_coords;
-        out vec3 v_normal;
-        out vec3 v_position;
-        out vec2 v_tex_coords;
-        uniform mat4 perspective;
-        uniform mat4 view;
-        uniform mat4 model;
-        void main() {
-            v_tex_coords = tex_coords;
-            mat4 modelview = view * model;
-            v_normal = transpose(inverse(mat3(modelview))) * normal;
-            gl_Position = perspective * modelview * vec4(position, 1.0);
-            v_position = gl_Position.xyz / gl_Position.w;
-        }
-    "#;
+    let vertex_shader_src = filesystem::open("./test/vertex_shader");
+    let vertex_shader_src_slice: &str = &vertex_shader_src;
+    let fragment_shader_src = filesystem::open("./test/fragment_shader");
+    let fragment_shader_src_slice: &str = &fragment_shader_src;
 
-    let fragment_shader_src = r#"
-        #version 140
-        in vec3 v_normal;
-        in vec3 v_position;
-        in vec2 v_tex_coords;
-        out vec4 color;
-        uniform vec3 u_light;
-        uniform sampler2D diffuse_tex;
-        uniform sampler2D normal_tex;
-        const vec3 specular_color = vec3(1.0, 1.0, 1.0);
-        mat3 cotangent_frame(vec3 normal, vec3 pos, vec2 uv) {
-            vec3 dp1 = dFdx(pos);
-            vec3 dp2 = dFdy(pos);
-            vec2 duv1 = dFdx(uv);
-            vec2 duv2 = dFdy(uv);
-            vec3 dp2perp = cross(dp2, normal);
-            vec3 dp1perp = cross(normal, dp1);
-            vec3 T = dp2perp * duv1.x + dp1perp * duv2.x;
-            vec3 B = dp2perp * duv1.y + dp1perp * duv2.y;
-            float invmax = inversesqrt(max(dot(T, T), dot(B, B)));
-            return mat3(T * invmax, B * invmax, normal);
-        }
-        void main() {
-            vec3 diffuse_color = texture(diffuse_tex, v_tex_coords).rgb;
-            vec3 ambient_color = diffuse_color * 0.1;
-            vec3 normal_map = texture(normal_tex, v_tex_coords).rgb;
-            mat3 tbn = cotangent_frame(v_normal, v_position, v_tex_coords);
-            vec3 real_normal = normalize(tbn * -(normal_map * 2.0 - 1.0));
-            float diffuse = max(dot(real_normal, normalize(u_light)), 0.0);
-            vec3 camera_dir = normalize(-v_position);
-            vec3 half_direction = normalize(normalize(u_light) + camera_dir);
-            float specular = pow(max(dot(half_direction, real_normal), 0.0), 16.0);
-            color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
-        }
-    "#;
-
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src,
+    let program = glium::Program::from_source(&display, vertex_shader_src_slice, fragment_shader_src_slice,
                                               None).unwrap();
 
     loop {
