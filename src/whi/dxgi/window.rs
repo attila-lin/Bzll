bc34b09837e347dee5e5597a2783c3329179184f
Yiyu Lin <linyiyu1992@gmail.com>
2017-02-18 12:44:47 +0800

    ad appveyor build script

--- src/whi/dxgi/window.rs
+++ src/whi/dxgi/window.rs
@@ -8,8 +8,13 @@ pub fn init() -> Result<(Window), InitError>{
         Ok(w) => w,
         Err(_) => return Err(InitError::Window),
     };
+    let (width, height) = inner.get_inner_size_pixels().unwrap();
+
+    let swap_chain =
+
     let window = Window{
         inner: inner,
+        swap_chain: swap_chain,
     };
 
     return Ok((window));
